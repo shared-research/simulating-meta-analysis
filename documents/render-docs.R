961b97e92fe3257ee3b8e7a6d8c22934a6d8bfdc
Filippo Gambarota <filippo.gambarota@gmail.com>
2023-09-26 19:38:34 +0200

    cleaning extra steps

--- documents/render-docs.R
+++ documents/render-docs.R
@@ -10,3 +10,20 @@ rmarkdown::render(input = "documents/paper/paper.Rmd",
 rmarkdown::render(input = "documents/paper/suppl.Rmd", 
                   output_dir = "documents/output",
                   output_file = "suppl.pdf")
+
+# preprint
+
+create_preprint()
+
+# submission
+
+# ## copy the paper
+# fs::file_copy("documents/paper/paper.Rmd",
+#               "documents/submission/paper.Rmd")
+# 
+# ## copy the cache
+# fs::dir_copy("documents/paper/paper_cache/",
+#              "documents/submission/")
+
+rmarkdown::render(input = "documents/submission/paper.Rmd",
+                  params = list(submission = TRUE))
